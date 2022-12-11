ff9ce7f16fa9e725bbbb9d39edf3069549ae3f61
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2022-12-11 18:37:31 -0500

    Empezar ejercicios de operaciones con cadenas

---
+++ Ejercicios_Operaciones_Cadenas/Program.cs
@@ -0,0 +1,31 @@
+﻿/*#region 
+
+Console.WriteLine("Escribe tu nombre: ");
+string nombre = Console.ReadLine().ToLower();
+
+while (nombre != "santiago")
+{
+    Console.WriteLine("Escribe otro nombre: ");
+    nombre = Console.ReadLine().ToLower();
+}
+
+Console.WriteLine("Tú nombre es Santiago, el mejor nombre del mundo");
+
+#endregion
+
+#region
+
+Console.WriteLine("Escribe una palabra: ");
+string palabra = Console.ReadLine().ToLower();
+string palabraInversa = "";
+
+
+for (int i = palabra.Length - 1; i >= 0 ; i--)
+{
+    palabraInversa += palabra[i];
+}
+
+Console.WriteLine("La palabra inversa es: " + palabraInversa);
+
+
+#endregion*/
