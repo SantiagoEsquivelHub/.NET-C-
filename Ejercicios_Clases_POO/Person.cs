25490cedd5c3f0b004d1c0338bf5dca3da0113c0
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2022-12-15 17:12:39 -0500

    Empezar ejercicios de clases en programación orientada a objetos

---
+++ Ejercicios_Clases_POO/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_Clases_POO
+{
+    internal class Person
+    {
+        public int age { get; set; }
+        public string name { get; set; }
+
+        public Person(int age, string name) 
+        { 
+            this.age = age;
+            this.name = name;
+        }
+
+        ~Person() { }
+    }
+}
---
+++ Ejercicios_Clases_POO/Program.cs
@@ -0,0 +1,59 @@
+﻿#region
+
+using Ejercicios_Clases_POO;
+using System.Xml.Linq;
+
+Console.WriteLine("We need the age and name of 5 people");
+Console.ReadLine();
+
+int peopleCounter = 1;
+List<Person> people = new List<Person>();
+
+while (peopleCounter <= 5)
+{
+
+    Console.WriteLine("Enter the data of a person");
+    Console.WriteLine("Age: ");
+    int age;
+    int.TryParse(Console.ReadLine(), out age);
+
+    while (age <= 0)
+    {
+        Console.WriteLine("Enter a valid age");
+        Console.WriteLine("Age: ");
+        age = int.Parse(Console.ReadLine());
+    }
+
+    Console.WriteLine("Name: ");
+    string name = Console.ReadLine();
+
+    while (name.Length < 2)
+    {
+        Console.WriteLine("Enter a valid name");
+        Console.WriteLine("Name: ");
+        name = Console.ReadLine();
+    }
+
+    Person person = new Person(age, name);
+    people.Add(person);
+    peopleCounter++;
+}
+
+Console.WriteLine("-------------------------<ADULTS>-------------------------");
+foreach (var person in people)
+{
+    if (person.age >= 18)
+    {
+        Console.WriteLine("-------------------------<PERSON>-------------------------");
+        Console.WriteLine("Name: " + person.name);
+        Console.WriteLine("Age: " + person.age);
+        Console.WriteLine("-------------------------</PERSON>-------------------------");
+    }
+}
+Console.WriteLine("-------------------------</ADULTS>-------------------------");
+
+
+
+
+
+#endregion
